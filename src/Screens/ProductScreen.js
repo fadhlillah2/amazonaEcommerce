e8c213e8394f5313fe095609b8fe2324a70d19e8
fadhlillah2 <fadhlillah949699@gmail.com>
2021-03-29 08:16:53 +0700

    two

---
+++ src/Screens/ProductScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import data from "../data";
+
+function ProductScreen(props) {
+  console.log(props.match.params.id);
+  const product = data.products.find((x) => x._id === props.match.params.id);
+  return (
+    <div>
+      <h1>{product.name}</h1>
+    </div>
+  );
+}
+export default ProductScreen;
